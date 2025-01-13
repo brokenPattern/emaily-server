be9a147ff48b6f528da8f8b5cbf8a85ceb4755bd
brokenPattern <michal.neumann@yahoo.pl>
2025-01-13 12:55:28 +0100

    Logout implemented

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 
 module.exports = (app) => {
 
-    app.get('/auth/google/', passport.authenticate(
+    app.get('/auth/google', passport.authenticate(
         'google',
         {
             scope: ['profile', 'email'],
@@ -11,7 +11,12 @@ module.exports = (app) => {
     
     app.get('/auth/google/callback', passport.authenticate('google'));
 
-    app.get('/api/current_user/', (req, res) => {
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.send(req.user);
+    });
+
+    app.get('/api/current_user', (req, res) => {
         res.send(req.user)
     });
 
